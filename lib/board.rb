be03886a8acde43a9b27bc787685fbbf78918b02
carisaelam <carisaelam@gmail.com>
2024-07-12 12:02:52 -0400

    Add hash to store unicodes for chess pieces

--- lib/board.rb
+++ lib/board.rb
@@ -8,6 +8,21 @@ class Board
   DARK_COLOR = 46
   LIGHT_COLOR = 47
 
+  CHESS_PIECES = {
+    black_king: "\u265A",
+    black_queen: "\u265B",
+    black_rook: "\u265C",
+    black_bishop: "\u265D",
+    black_knight: "\u265E",
+    black_pawn: "\u265F",
+    white_king: "\u2654",
+    white_queen: "\u2655",
+    white_rook: "\u2656",
+    white_bishop: "\u2657",
+    white_knight: "\u2658",
+    white_pawn: "\u2659"
+  }
+
   def initialize
     @board = initialize_board
   end
@@ -29,6 +44,10 @@ class Board
 
   def light
     LIGHT_COLOR
+  end
+
+  def piece_codes
+    CHESS_PIECES
   end
 
   def print_square(square)
@@ -66,6 +85,13 @@ class Board
   end
 end
 
-# board = Board.new
-# pp board
-# board.draw_board
+board = Board.new
+board.board[0][0][1] = board.piece_codes[:white_bishop]
+board.board[0][1][1] = board.piece_codes[:white_queen]
+board.board[1][0][1] = board.piece_codes[:black_knight]
+board.board[1][1][1] = board.piece_codes[:black_pawn]
+
+board.draw_board
+
+puts "\u265A"  # Should display the white king correctly
+puts "\u2654"  # Should display the black king correctly
