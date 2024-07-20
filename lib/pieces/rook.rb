9267c95bced4475a281151fbeaa4bcbacfeb611c
carisaelam <carisaelam@gmail.com>
2024-07-20 06:27:54 -0400

    Clean up rook class

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -15,120 +15,42 @@ class Rook < Piece
   end
 
   def valid_move?(start_position, end_position)
-    p 'VALID MOVE RUNNING inside ROOK'
-    row = start_position[0]
-    col = start_position[1]
+    row, col = start_position
 
-    left_moves = [
-      [row, col + 1],
-      [row, col + 2],
-      [row, col + 3],
-      [row, col + 4],
-      [row, col + 5],
-      [row, col + 6],
-      [row, col + 7]
-    ]
+    # last two numbers describe how row/col changes
+    right_moves = generate_moves(row, col, 0, 1)
+    left_moves = generate_moves(row, col, 0, -1)
+    down_moves = generate_moves(row, col, 1, 0)
+    up_moves = generate_moves(row, col, -1, 0)
 
-    right_moves = [
-      [row, col - 1],
-      [row, col - 2],
-      [row, col - 3],
-      [row, col - 4],
-      [row, col - 5],
-      [row, col - 6],
-      [row, col - 7]
-    ]
+    all_moves = left_moves + right_moves + down_moves + up_moves
 
-    down_moves = [
-      [row + 1, col],
-      [row + 2, col],
-      [row + 3, col],
-      [row + 4, col],
-      [row + 5, col],
-      [row + 6, col],
-      [row + 7, col]
-    ]
-    up_moves = [
-      [row - 1, col],
-      [row - 2, col],
-      [row - 3, col],
-      [row - 4, col],
-      [row - 5, col],
-      [row - 6, col],
-      [row - 7, col]
-    ]
-
-    def in_bounds?(position)
-      p "running in_bounds for position: #{position}"
-      p position[0].between?(0, 7) && position[1].between?(0, 7)
-    end
-
-    def occupied_by_my_piece?(position)
-    end
-
-    moves = []
-    down_moves.each do |move|
-      p 'DOWN move'
-      p "move is #{move}"
-      break unless in_bounds?(move)
-
-      piece = @board.piece_at(move)
-      break if color == piece.color
-
-      moves << move
-    end
-
-    up_moves.each do |move|
-      p 'UP move'
-      p "move is #{move}"
-      break unless in_bounds?(move)
-
-      piece = @board.piece_at(move)
-      break if color == piece.color
-
-      moves << move
-    end
-
-    left_moves.each do |move|
-      p 'LEFT move'
-      p move
-
-      break unless in_bounds?(move)
-
-      piece = @board.piece_at(move)
-      break if color == piece.color
-
-      moves << move
-    end
-
-    right_moves.each do |move|
-      p 'RIGHT move'
-      p move
-
-      break unless in_bounds?(move)
-
-      piece = @board.piece_at(move)
-      break if color == piece.color
-
-      moves << move
-    end
-
-    p "valid moves include: #{moves}"
-
-    filtered_moves = filter_possible_moves(moves)
-    filtered_moves.include?(end_position)
+    # ultimately says whether move is valid
+    all_moves.include?(end_position)
   end
 
-  # filter out out of bounds moves
-  def filter_possible_moves(moves)
-    legal_moves = []
+  private
 
-    moves.each do |move|
-      next unless move[0].between?(0, 7) && move[1].between?(0, 7)
+  def generate_moves(row, col, row_change, col_change)
+    moves = []
+    (1..7).each do |step|
+      new_row = row + step * row_change
+      new_col = col + step * col_change
+      next_move = [new_row, new_col]
 
-      legal_moves.push(move)
+      break unless in_bounds?(next_move)
+
+      piece = @board.piece_at(next_move)
+      break if color == piece.color
+
+      moves << next_move
+      break unless piece.color == :empty
     end
+    p "valid moves include #{moves}"
+    moves
+  end
 
-    legal_moves
+  def in_bounds?(position)
+    position.all? { |coord| coord.between?(0, 7) }
   end
 end
